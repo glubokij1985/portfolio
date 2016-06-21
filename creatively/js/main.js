645de75a807e9ac12caaef1a455c611f7eb9240e
glubokij1985 <glubokij85@gmail.com>
2016-06-21 23:39:44 +0300

    creatively

--- creatively/js/main.js
+++ creatively/js/main.js
@@ -1,38 +1,38 @@
-jQuery(function(){
+$(function(){
 //======================= CAROUSEL-HEAD ===========================                                   
-	jQuery('.carousel-head').flexslider({
+	$('.carousel-head').flexslider({
 	    animation: "slide",
 	    slideshowSpeed: 5000,
 	    pauseOnAction: true,
 	    pauseOnHover: true,
   	});
 //======================= CAROUSEL SECOND =========================== 
-  	jQuery('.carousel-monitor').flexslider({
+  	$('.carousel-monitor').flexslider({
 	    animation: "fade",
 	    slideshowSpeed: 5000,
 	    pauseOnAction: true,
 	    pauseOnHover: true,
   	});
 //======================= BURGER-BUTTON =========================== 
-  	jQuery('.burger-btn-holder').on('click', function(){
-		if (!jQuery(this).hasClass('active')){
-			jQuery(this).addClass('active');
-			jQuery(this).next().stop(true, true).slideDown('normal');
+  	$('.burger-btn-holder').on('click', function(){
+		if (!$(this).hasClass('active')){
+			$(this).addClass('active');
+			$(this).next().stop(true, true).slideDown('normal');
 		} else {
-			jQuery(this).removeClass('active');
-			jQuery(this).next().stop(true, true).slideUp('normal');
+			$(this).removeClass('active');
+			$(this).next().stop(true, true).slideUp('normal');
 		}
 	});
 //======================= GO-TO-TOP =========================== 
-    jQuery(window).scroll(function () {
-        if (jQuery(this).scrollTop() > 0) {
-            jQuery('.flex-go-to-top').fadeIn();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.flex-go-to-top').fadeIn();
         } else {
-            jQuery('.flex-go-to-top').fadeOut();
+            $('.flex-go-to-top').fadeOut();
         }
     });
-    jQuery('.flex-go-to-top').click(function () {
-        jQuery('body,html').animate({
+    $('.flex-go-to-top').click(function () {
+        $('body,html').animate({
             scrollTop: 0
         }, 400);
         return false;
